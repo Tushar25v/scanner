360a1a0f8046a12a2d7c4c7299e6ea66dc8dcba1
Tushar25v <107396077+Tushar25v@users.noreply.github.com>
2022-08-01 08:57:37 +0530

    Added home page and reterived the nsv data

--- website/views.py
+++ website/views.py
@@ -9,6 +9,9 @@ views = Blueprint('views', __name__)
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    return render_template("home.html", user=current_user)
+    return render_template("Table.htm", user=current_user)
 
-
+@views.route('/index',methods=['GET','POST'])
+@login_required
+def index():
+    return render_template("index.html",user=current_user)
